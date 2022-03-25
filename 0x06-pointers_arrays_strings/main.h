4a3bedd863a01c014ada1664f13649f9f95d8090
IGOHOZO2002 <99493411+IGOHOZO2002@users.noreply.github.com>
2022-03-25 14:59:45 -0700

    Task 9

--- 0x06-pointers_arrays_strings/main.h
+++ 0x06-pointers_arrays_strings/main.h
@@ -1,3 +1,3 @@
-char *_strcat(char *dest, char *src);
-char *_strncat(char *dest, char *src, int n);
-char *_strncpy(char *dest, char *src, int n);
+[main e7589dd] my pointers_arrays_strings
+ 2 files changed, 6 insertions(+), 1 deletion(-)
+ create mode 100644 0x06-pointers_arrays_strings/main.h
