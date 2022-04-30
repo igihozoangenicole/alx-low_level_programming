901a7167239e0645a6a8aa222354f8596ab287f5
IGOHOZO2002 <99493411+IGOHOZO2002@users.noreply.github.com>
2022-04-30 02:25:09 -0700

    Print_chessboard

--- 0x07-pointers_arrays_strings/main.h
+++ 0x07-pointers_arrays_strings/main.h
@@ -8,3 +8,22 @@ char *_strstr(char *haystack, char *needle);
 void print_chessboard(char (*a)[8]);
 void print_diagsums(int *a, int size);
 void set_string(char **s, char *to);
+On branch main
+Your branch is ahead of 'origin/main' by 1 commit.
+  (use "git push" to publish your local commits)
+
+Changes not staged for commit:
+  (use "git add <file>..." to update what will be committed)
+  (use "git restore <file>..." to discard changes in working directory)
+	modified:   ../0x08-recursion/main.h
+	modified:   ../0x0A-argc_argv/README.md
+	modified:   ../0x0B-malloc_free/main.h
+	modified:   ../0x0C-more_malloc_free/README.md
+	modified:   ../0x14-bit_manipulation/main.h
+
+Untracked files:
+  (use "git add <file>..." to include in what will be committed)
+	../0x09-static_libraries/.libmy.c.swp
+	../libmy (5).a
+
+no changes added to commit (use "git add" and/or "git commit -a")
